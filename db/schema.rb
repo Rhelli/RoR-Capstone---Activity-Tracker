89c89576f0d7a03efa0101ecc679afdf395a562c
Rory Hellier <rory.hellier@googlemail.com>
2020-06-15 17:21:59 +0100

    Added Groups Model
     - Added name and description columns

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_15_161336) do
+ActiveRecord::Schema.define(version: 2020_06_15_162051) do
 
   create_table "activities", force: :cascade do |t|
     t.string "name"
     t.integer "amount"
     t.integer "calories"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "groups", force: :cascade do |t|
+    t.string "name"
+    t.string "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
