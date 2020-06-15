20dc7b8787f5f202bc67ab777b07af3cd9f3a764
Rory Hellier <rory.hellier@googlemail.com>
2020-06-15 19:30:09 +0100

    Minor Formatting Changes to User Model
     - Removed double quotations from strings
     - Froze regex assignments

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,12 +2,12 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
-  
+
   validates :password, presence: true, length: { in: 7..32 }, confirmation: { case_sensitive: true }
   validates :password_confirmation, presence: true
-  VALID_NAME_REGEX = /\A[a-zA-Z]+\z/
-  validates :first_name, presence: true, length: { in: 1..50 }, format: { with: VALID_NAME_REGEX, message: "only allows letters" }
-  validates :last_name, presence: true, length: { in: 1..50 }, format: { with: VALID_NAME_REGEX, message: "only allows letters" }
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  VALID_NAME_REGEX = [/\A[a-zA-Z]+\z/].freeze
+  validates :first_name, presence: true, length: { in: 1..50 }, format: { with: VALID_NAME_REGEX, message: 'only allows letters' }
+  validates :last_name, presence: true, length: { in: 1..50 }, format: { with: VALID_NAME_REGEX, message: 'only allows letters' }
+  VALID_EMAIL_REGEX = [/\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i].freeze
   validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
 end
