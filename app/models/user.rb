9dca79042501b8c305664f14b7d5b44d10f1272b
Rory Hellier <rory.hellier@googlemail.com>
2020-06-15 19:26:05 +0100

    Validations & Tests for User Model Complete
     - Added validation for password (presence, length & confirmation)
     - Added validation for password_confirmation (presence)
     - Added validation for user first_name (presence, length, name regex (only letters))
     - Added same validations for last_name
     - Added valid regex for email
     - Added validations for email (presence, length, valid regex format)

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,11 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
+
+  validates :password_confirmation, presence: true
+  validates :password, presence: true, length: { in: 7..32 }, confirmation: { case_sensitive: true }
+  validates :first_name, presence: true, length: { in: 1..50 }, format: { with: /\A[a-zA-Z]+\z/, message: "only allows letters" }
+  validates :last_name, presence: true, length: { in: 1..50 }, format: { with: /\A[a-zA-Z]+\z/, message: "only allows letters" }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -43,12 +43,32 @@ RSpec.describe User, type: :model do
     end
 
     it 'rejects new users with too long an email address' do
-      samwise = User.new(first_name: 'Samwise', last_name: 'Gamgee', email: 's' * 38 + '@hobbiton.com', password: 'password', password_confirmation: 'password')
+      samwise = User.new(first_name: 'Samwise', last_name: 'Gamgee', email: 's' * 243 + '@hobbiton.com', password: 'password', password_confirmation: 'password')
       expect(samwise).to_not be_valid
     end
 
     it 'rejects new users without any email at all' do
       samwise = User.new(first_name: 'Samwise', last_name: 'Gamgee', email: nil, password: 'password', password_confirmation: 'password')
+      expect(samwise).to_not be_valid
+    end
+
+    it 'rejects new users without a password' do
+      samwise = User.new(first_name: 'Samwise', last_name: 'Gamgee', email: 'samwise@hobbiton.com', password: nil, password_confirmation: 'password')
+      expect(samwise).to_not be_valid
+    end
+
+    it 'rejects new users without a password confirmation' do
+      samwise = User.new(first_name: 'Samwise', last_name: 'Gamgee', email: 'samwise@hobbiton.com', password: 'password', password_confirmation: nil)
+      expect(samwise).to_not be_valid
+    end
+
+    it 'rejects new users with passwords which are too long' do
+      samwise = User.new(first_name: 'Samwise', last_name: 'Gamgee', email: 'samwise@hobbiton.com', password: 'p' * 33, password_confirmation: 'p' * 129)
+      expect(samwise).to_not be_valid
+    end
+
+    it 'rejects new users with passwords which are too short' do
+      samwise = User.new(first_name: 'Samwise', last_name: 'Gamgee', email: 'samwise@hobbiton.com', password: 'p' * 6, password_confirmation: 'p' * 6)
       expect(samwise).to_not be_valid
     end
   end
