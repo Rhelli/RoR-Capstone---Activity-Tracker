40fa9405e3d2baa951178120faba66e0e373b03d
Rory Hellier <rory.hellier@googlemail.com>
2020-06-17 13:46:41 +0100

    Added Basic Structure and Style to The User Show Page
     - Structured the page into bootstrap columns
     - One column for the user profile and details
     - Another column for all other options. Will most likely act as the render area for changing account information

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,11 @@
 module UsersHelper
+  def full_name
+    "#{current_user.first_name} #{current_user.last_name}"
+  end
+
+  def gravatar_for(user)
+    gravatar_id = Digest::MD5.hexdigest(user.email.downcase)
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?d=identicon&s=400"
+    image_tag(gravatar_url, alt: user.first_name.titleize, class: 'gravatar')
+  end
 end
