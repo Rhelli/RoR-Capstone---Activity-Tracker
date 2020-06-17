5c604c4163f38ec1ca88009ebd562024915daf55
Rory Hellier <rory.hellier@googlemail.com>
2020-06-17 12:06:11 +0100

    Navbar and Devise Updates
     - Moved navbar into partial
     - Navbar only appears when current_user is present now
     - Modified the devise security parameters in the app controller to accept first_name and last_name parameters

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :password, :password_confirmation) }
-    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :email, :password, :current_password) }
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:first_name, :last_name, :email, :password, :password_confirmation) }
+    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:first_name, :last_name, :email, :password, :current_password) }
   end
 end
